8f0001a473f16f01a748d54e8b98454a80fb8420
Thomas Page <thomaspage33@gmail.com>
2020-08-22 19:03:16 +1200

    Hidden token

--- vegebot.py
+++ vegebot.py
@@ -9,7 +9,13 @@ async def on_ready():
     print("Bot is ready")
 
 @bot.command()
-async def help(ctx):
+async def helpplease(ctx):
     await ctx.send("0800 543 354")
 
-bot.run("NzQ2NjA1NzY5NjQxOTUxMzEz.X0Cwzg.b2jXUMwWG38VgMfzHiHvx70jtAk")
+# Read bot token
+# Create a token.txt with your own token if you want to use this bot
+token_file = open("token.txt", "r")
+
+bot.run(token_file.read())
+
+token_file.close()
