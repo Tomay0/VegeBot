097dca4aff62e286fd5a87ec045189eefd852ecc
Tomay0 <thomaspage33@gmail.com>
2021-10-27 21:10:33 +1300

    changed to use external database

--- src/database.py
+++ src/database.py
@@ -18,35 +18,10 @@ def wait_for_postgrest(num_tries=20):
                 raise RuntimeError("Could not connect to PostgREST")
 
 
-def get_database(num_tries=20):
-    n = 0
-    while True:
-        try:
-            return psycopg2.connect(
-                host=getenv('POSTGRES_HOST'),
-                database=getenv('POSTGRES_DB'),
-                user=getenv('POSTGRES_USER'),
-                password=getenv('POSTGRES_PASSWORD'))
-        except psycopg2.errors.OperationalError:
-            sleep(1)
-            n += 1
-            if n >= num_tries:
-                raise RuntimeError("Could not connect to Postgres")
-
-
 async def reset_database(client, database):
     if database is None:
         logging.warning('Database config is not set, so could not be loaded')
         return
-
-    conn = get_database()
-
-    cur = conn.cursor()
-    with open('init.sql') as sql_init_file:
-        cur.execute(sql_init_file.read())
-    conn.commit()
-    cur.close()
-    conn.close()
 
     wait_for_postgrest()
     database.clear_database()
@@ -86,22 +61,22 @@ class PostgRESTDatabase:
     def clear_database(self):
         headers = {'Authorization': f'Bearer {self.token}', 'Content-Type': 'application/json'}
 
-        response = requests.delete(f'{self.url}/discordmessages', headers=headers, json={})
+        response = requests.delete(f'{self.url}/messages', headers=headers, json={})
 
         if response.status_code != 204:
             logging.error(f"Error {response.status_code} deleting messages:\n{response.text}")
 
-        response = requests.delete(f'{self.url}/discordchannels', headers=headers, json={})
+        response = requests.delete(f'{self.url}/channels', headers=headers, json={})
 
         if response.status_code != 204:
             logging.error(f"Error {response.status_code} deleting channels:\n{response.text}")
 
-        response = requests.delete(f'{self.url}/discordguilds', headers=headers, json={})
+        response = requests.delete(f'{self.url}/guilds', headers=headers, json={})
 
         if response.status_code != 204:
             logging.error(f"Error {response.status_code} deleting guilds:\n{response.text}")
 
-        response = requests.delete(f'{self.url}/discordusers', headers=headers, json={})
+        response = requests.delete(f'{self.url}/users', headers=headers, json={})
 
         if response.status_code != 204:
             logging.error(f"Error {response.status_code} deleting users:\n{response.text}")
@@ -110,9 +85,9 @@ class PostgRESTDatabase:
         headers = {'Authorization': f'Bearer {self.token}', 'Content-Type': 'application/json',
                    'Prefer': 'params=multiple-objects'}
 
-        content = [{'g_id': message.guild.id, 'g_name': message.guild.name, 'c_id': message.channel.id,
-                    'c_name': message.channel.name, 'u_id': message.author.id, 'u_name': message.author.name,
-                    'msg_id': message.id, 'msg': clean_message(message.content),
+        content = [{'g_id': str(message.guild.id), 'g_name': message.guild.name, 'c_id': str(message.channel.id),
+                    'c_name': message.channel.name, 'u_id': str(message.author.id), 'u_name': message.author.name,
+                    'msg_id': str(message.id), 'msg': clean_message(message.content),
                     'msg_timestamp': message.created_at.strftime("%Y-%m-%d %H:%M:%S")} for message in messages]
 
         response = requests.post(f'{self.url}/rpc/add_message', headers=headers, json=content)
@@ -123,9 +98,9 @@ class PostgRESTDatabase:
     def add_message(self, message):
         headers = {'Authorization': f'Bearer {self.token}', 'Content-Type': 'application/json'}
 
-        content = {'g_id': message.guild.id, 'g_name': message.guild.name, 'c_id': message.channel.id,
-                   'c_name': message.channel.name, 'u_id': message.author.id, 'u_name': message.author.name,
-                   'msg_id': int(message.id), 'msg': clean_message(message.content),
+        content = {'g_id': str(message.guild.id), 'g_name': message.guild.name, 'c_id': str(message.channel.id),
+                   'c_name': message.channel.name, 'u_id': str(message.author.id), 'u_name': message.author.name,
+                   'msg_id': str(message.id), 'msg': clean_message(message.content),
                    'msg_timestamp': message.created_at.strftime("%Y-%m-%d %H:%M:%S")}
 
         response = requests.post(f'{self.url}/rpc/add_message', headers=headers, json=content)
@@ -136,7 +111,7 @@ class PostgRESTDatabase:
     def delete_message(self, message):
         headers = {'Authorization': f'Bearer {self.token}', 'Content-Type': 'application/json'}
 
-        response = requests.delete(f'{self.url}/discordmessages?message_id=eq.{message.id}', headers=headers, json={})
+        response = requests.delete(f'{self.url}/messages?id=eq.{message.id}', headers=headers, json={})
 
         if response.status_code != 204:
             logging.error(f"Error {response.status_code} deleting message:\n{response.text}")
